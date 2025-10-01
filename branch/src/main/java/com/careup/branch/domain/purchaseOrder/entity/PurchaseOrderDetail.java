4dd084ea33e761462d92bf2270cb201c09b4a1b6
SeungJi20 <lina6709@naver.com>
2025-10-01 15:12:02 +0900

    fix: 발주관련 엔티티 수정 #15

--- branch/src/main/java/com/careup/branch/domain/purchaseOrder/entity/PurchaseOrderDetail.java
+++ branch/src/main/java/com/careup/branch/domain/purchaseOrder/entity/PurchaseOrderDetail.java
@@ -3,6 +3,7 @@ package com.careup.branch.domain.purchaseOrder.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 public class PurchaseOrderDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
