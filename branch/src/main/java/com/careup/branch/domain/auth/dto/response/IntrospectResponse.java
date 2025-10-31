1a09da0d20aabaf7b78f6190b4824ffab48f75ff
SeungJi20 <lina6709@naver.com>
2025-10-31 09:18:43 +0900

    feat: 사이드바 지점명 표시 #15

--- branch/src/main/java/com/careup/branch/domain/auth/dto/response/IntrospectResponse.java
+++ branch/src/main/java/com/careup/branch/domain/auth/dto/response/IntrospectResponse.java
@@ -13,6 +13,7 @@ public class IntrospectResponse {
     private boolean active;
     private Long employeeId;
     private Long branchId;
+    private String branchName;
     private String role;
     private Long iat; // seconds
     private Long exp; // seconds
