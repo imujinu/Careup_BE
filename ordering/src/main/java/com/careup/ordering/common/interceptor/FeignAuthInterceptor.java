f51b4125caa095e595a1d5ea229d3480806c718d
pure-wa <tkdghks8242@gmail.com>
2025-11-03 03:42:26 +0900

    fix: 상품별 지점조회시 권한,토큰문제 수정

--- ordering/src/main/java/com/careup/ordering/common/interceptor/FeignAuthInterceptor.java
+++ ordering/src/main/java/com/careup/ordering/common/interceptor/FeignAuthInterceptor.java
@@ -2,10 +2,12 @@ package com.careup.ordering.common.interceptor;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class FeignAuthInterceptor implements RequestInterceptor {
 
