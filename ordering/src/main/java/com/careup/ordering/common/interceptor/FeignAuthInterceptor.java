ae520487e3190960669c0346e31ff1f33f2bcdf2
pure-wa <tkdghks8242@gmail.com>
2025-11-03 14:35:36 +0900

    fix: 코드 수정 및 복구

--- ordering/src/main/java/com/careup/ordering/common/interceptor/FeignAuthInterceptor.java
+++ ordering/src/main/java/com/careup/ordering/common/interceptor/FeignAuthInterceptor.java
@@ -2,12 +2,10 @@ package com.careup.ordering.common.interceptor;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class FeignAuthInterceptor implements RequestInterceptor {
 
