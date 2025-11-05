5119e75bc4eb83b6be94777a432efbeb30cd557a
pure-wa <tkdghks8242@gmail.com>
2025-11-05 09:26:56 +0900

    fix: 속성추가로 인한 인자

--- ordering/src/main/java/com/careup/ordering/domain/product/controller/InventoryController.java
+++ ordering/src/main/java/com/careup/ordering/domain/product/controller/InventoryController.java
@@ -262,16 +262,22 @@ public class InventoryController {
     }
 
     private InventoryFlowResponseDto convertToInventoryFlowResponse(InventoryFlowDetail flow) {
+        BranchProduct branchProduct = flow.getBranchProduct();
         return new InventoryFlowResponseDto(
                 flow.getId(),
-                flow.getBranchProduct().getId(),
-                flow.getBranchProduct().getProduct().getId(),
-                flow.getBranchProduct().getProduct().getName(),
-                flow.getBranchProduct().getBranchId(),
+                branchProduct.getId(),
+                branchProduct.getProduct().getId(),
+                branchProduct.getProduct().getName(),
+                branchProduct.getBranchId(),
                 flow.getInQuantity(),
                 flow.getOutQuantity(),
                 flow.getRemark(),
-                flow.getCreateAt().toString()
+                flow.getCreateAt().toString(),
+                // 사이즈 정보
+                branchProduct.getAttributeValue() != null ? branchProduct.getAttributeValue().getId() : null,
+                branchProduct.getAttributeValue() != null ? branchProduct.getAttributeValue().getDisplayName() : null,
+                branchProduct.getAttributeValue() != null && branchProduct.getAttributeValue().getAttributeType() != null 
+                    ? branchProduct.getAttributeValue().getAttributeType().getName() : null
         );
     }
 }
